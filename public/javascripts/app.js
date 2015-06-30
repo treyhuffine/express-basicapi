7ead5b87367074981da211d05cd0bc045b3ad148
Trey Huffine <treyhuffine@gmail.com>
2015-06-30 12:07:55 -0700

    Successfully pull JSON quotes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,14 @@
 var quotesApp = angular.module("quotesApp", []);
 
-quotesApp.controller("QuotesCtrl", function() {
-  
-})
+quotesApp.controller("QuotesCtrl", function($scope, $http) {
+  $scope.hello = "Hello world";
+  $scope.quotes = [];
+  $http.get("/quotes")
+    .success(function(data, status, headers, config) {
+      console.log("Data: ", data);
+      $scope.quotes = data.quotes;
+    })
+    .error(function(data, status, headers, config) {
+      console.log("error");
+    });
+});
