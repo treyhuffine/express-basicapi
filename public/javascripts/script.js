f394dff2db557ae4c971b16c2e0edffb0e373f4f
Trey Huffine <treyhuffine@gmail.com>
2015-06-30 17:51:10 -0700

    Get the D in crud

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,20 +5,35 @@ quotesApp.controller("QuotesCtrl", function($scope, $http) {
   $scope.quotes = [];
   $http.get("/quotes")
     .success(function(data, status, headers, config) {
-      console.log("Data: ", data);
       $scope.quotes = data.quotes;
     })
     .error(function(data, status, headers, config) {
       console.log("error");
   });
   $scope.submitQuote = function() {
+    $scope.formError = "";
     $http.post("/quotes", { quote: $scope.newQuote })
       .success(function(data, status, headers, config) {
         $scope.quotes.push(data.quote);
         $scope.newQuote = "";
       })
-      .error(function(data, status, headers, config) {
-        console.log("Error");
+      .catch(function(err) {
+        console.log(err);
+        $scope.formError = err.data;
       });
+  };
+  $scope.deleteQuote = function(idx) {
+    var delPost = confirm("Delete post?");
+    if (delPost) {
+      //$scope.quotes.splice(idx,1);
+      $http.delete("/quotes/" + idx)
+        .success(function(data, status, headers, config) {
+          $scope.quotes.splice(data.deletedIdx, 1);
+        })
+        .catch(function(err) {
+          console.log(err);
+          $scope.formError = err.data;
+        });
+    }
   };
 });
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,17 @@ var fs = require('fs');
 // var path = require('path');
 var jsonData = require('../jsonData.json');
 
+var persistData = function(callbacks) {
+   fs.writeFile("jsonData.json", JSON.stringify(jsonData), function(err){
+      if (err) {
+        console.log(err);
+        callbacks.error();
+      }
+      callbacks.success();
+    });
+};
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', jsonData );
@@ -17,18 +28,51 @@ router.get('/quotes', function(req, res) {
 router.post('/quotes', function(req, res) {
   var newQuote = req.body.quote;
   if (!newQuote || jsonData.quotes.indexOf(newQuote) > -1) {
-    res.status(400).send("We need a text for that quote, son");
+    res.status(400).send("Error: write something new");
     return;
   }
   jsonData.quotes.push(req.body.quote);
-  fs.writeFile("jsonData.json", JSON.stringify(jsonData), function(err){
-    if (err) {
-      console.log(err);
-      res.status(500).send("We could not save the quote, please try again later");
-    }
-    res.json({quote: newQuote});
-  });
+  persistData({
+    success: function() {
+     //on success
+     res.json({ quote: newQuote });
+    },
+    error: function() {
+     //on error
+     res.status(500).json( { error: "We could not save the quote, please try again later" } );
+  }});
 });
+router.delete('/quotes/:id', function(req, res) {
+  var id = req.params.id,
+      spliced = jsonData.quotes.splice(id, 1);
+  if (spliced === 0) {
+    res.status(404).send("Error: quote doesn't exist");
+    return;
+  }
+  persistData({
+    success: function() {
+      res.json({message: "quote deleted", deletedIdx: id});
+    },
+    error: function() {
+      res.status(400).json({message: "quote was not deleted"});
+  }});
+});
+router.put('/quotes/:id', function(req, res) {
+  var id = req.params.id;
+  if (id > jsonData.quotes.length-1) {
+    res.status(404).json({error: "Error: quote doesn't exist"});
+    return;
+  }
+  jsonData.quotes = req.body.quote;
+  persistData({
+    success: function() {
+      res.json({message: "quote deleted"});
+    },
+    error: function() {
+      res.status(400).json({message: "quote was not delete"});
+  }});
+});
+
 router.get('/data/:id', function(req, res) {
   res.send(jsonData.quotes[req.params.id-1]);
 });
